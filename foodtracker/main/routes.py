fcbc1425d8a79428548316837b23c2271beb99bb
Jason Yang <canyoncrestjy@gmail.com>
2021-06-18 14:41:38 -0700

    giant commit

--- foodtracker/main/routes.py
+++ foodtracker/main/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from foodtracker.models import Food
+from foodtracker.extensions import db
+
 
 main = Blueprint('main', __name__)
 
@@ -9,6 +12,28 @@ def index():
 @main.route('/add')
 def add():
     return render_template('add.html')
+
+@main.route('/add', methods=['POST'])
+def add_post():
+    food_name = request.form.get('food-name')
+    proteins = request.form.get('protein')
+    fats = request.form.get('fat')
+    carbs = request.form.get('carbohydrates')
+
+    #code here basically contols all the functionality of the add feature
+
+    new_food = Food(name = food_name, 
+        proteins = proteins, 
+        carbs =carbs, 
+        fats=fats)
+
+    # db.session.add(new_food)
+    # db.session.commit()
+
+    return f'<h1>{ food_name } - { proteins} - { carbs } - { fats }<h1/>'
+
+    
+
 
 @main.route('/view')
 def view():
